e9f605c23cad0c74282897647b554964a72a352f
nurrative <nursul1997@gmail.com>
2025-01-07 23:12:03 +0600

    create posts model

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -4,9 +4,11 @@ __all__ = (
     "db_helper",
     "Product",
     "User",
+    "Post",
 )
 
 from .base import Base
 from .product import Product
 from .user import User
+from .post import Post
 from .db_helper import DatabaseHelper, db_helper
